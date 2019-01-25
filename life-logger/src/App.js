6ef79350066ea93dc6e09060e18a7d78bf957d77
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2019-01-25 16:44:45 -0500

    Use static properties on Component

--- life-logger/src/App.js
+++ life-logger/src/App.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+    testClick = () => {
+        console.log("hmmmmm");
+        // console.log(this.props);
+    };
+
     render() {
+        console.log(this.props);
         return (
-            <div className="App">
-                App
+            <div>
+                <h1 className="App">
+                    App
+                </h1>
+                <button onClick={this.testClick}>Click</button>
             </div>
         );
     }
 }
 
-const myTestWrapper = (WrappedComponent) => {
-    return class extends Component {
-        render() {
-            return (
-                <div style={{backgroundColor: "blue"}}>
-                    <WrappedComponent />
-                </div>
-            );
-        }
-    }
-};
+// const myTestWrapper = (WrappedComponent) => {
+//     return class extends Component {
+//         render() {
+//             return (
+//                 <div style={{backgroundColor: "blue"}}>
+//                     <WrappedComponent />
+//                 </div>
+//             );
+//         }
+//     }
+// };
+//
+// App = myTestWrapper(App);
 
-App = myTestWrapper(App);
+/*
+    Configure our own static properties.
+    Can also be written within the class using "static" keyword.
+*/
+App.displayName = "LoggerApp";
+App.defaultProps = {
+    data: []
+};
 
 export default App;
